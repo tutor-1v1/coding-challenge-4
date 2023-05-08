ddd50cca7043c952cb3c08dc62c5e9473491e3ac
tutor-1v1 <liumeng64@gmail.com>
2023-05-08 09:46:02 +0800

    WeChat cstutorcs coding challenge

---
+++ packages/app/next-env.d.ts
@@ -0,0 +1,13 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
---
+++ packages/react/.storybook/storybook-theme.ts
@@ -0,0 +1,18 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+import { create } from "@storybook/theming"
+// @ts-ignore
+import brandImage from "./storybook-logo.svg"
+
+export default create({
+  base: "light",
+  brandTitle: "Chakra UI",
+  brandUrl: "https://chakra-ui.com",
+  brandImage,
+})
